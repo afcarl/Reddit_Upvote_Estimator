76686309f983eee846a9ef3168a5c28e324247a4
Brendan J. Herger <b@revup.com>
2017-11-18 20:20:57 -0800

    Interface for Reddit

--- bin/reddit_scraper.py
+++ bin/reddit_scraper.py
@@ -8,4 +8,21 @@ Code Template
 
 
 def scrape_subreddit(subreddit, num_days):
+
+    # TODO Reference variables
+
+    # TODO Create connection
+
+    # TODO Find correct subreddit
+
+    # TODO Compute correct time range (current datetime - num_days to current datetime)
+
+    # TODO Iterate through posts chronologically
+
+    # TODO Add info from each post to aggregator
+
+    # TODO Create DataFrame from pulled data
+
+    # TODO Return
+
     pass
