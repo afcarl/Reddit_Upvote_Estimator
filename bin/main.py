60d383155f853265bbb38a0f9547245ec64a8600
Brendan J. Herger <b@revup.com>
2017-11-20 10:22:44 -0800

    Partial progress towards archiving model output

--- bin/main.py
+++ bin/main.py
@@ -72,9 +72,9 @@ def transform(embedding_matrix, word_to_index, observations):
 
     logging.info('End transform')
     lib.archive_dataset_schemas('transform', locals(), globals())
-    return embedding_matrix, word_to_index, observations
+    return embedding_matrix, word_to_index, observations, label_encoder
 
-def model(embedding_matrix, word_to_index, observations):
+def model(embedding_matrix, word_to_index, observations, label_encoder):
     # TODO Docstring
     logging.info('Begin model')
 
@@ -123,7 +123,7 @@ def model(embedding_matrix, word_to_index, observations):
         classification_model = models.gen_conv_model(embedding_input_length, output_shape, embedding_matrix, word_to_index)
 
         # Train model
-        classification_model.fit(x_train, y_train, batch_size=128, epochs=20, validation_data=(x_test, y_test))
+        classification_model.fit(x_train, y_train, batch_size=128, epochs=1, validation_data=(x_test, y_test))
 
         logging.info('Finished creating and training model')
     else:
@@ -132,16 +132,18 @@ def model(embedding_matrix, word_to_index, observations):
     # TODO Validate model
 
     # Add model prediction to observations
-    observations['modeling_prediction'] = classification_model.predict(observations['modeling_input'].tolist())
+    preds = classification_model.predict(numpy.array(observations['modeling_input'].tolist(), dtype=object))
+    observations['max_probability'] = map(max, preds)
+    observations['modeling_prediction'] = map(lambda x: lib.prop_to_label(x, label_encoder), preds)
 
     # Archive schema and return
     lib.archive_dataset_schemas('transform', locals(), globals())
     logging.info('End model')
 
-    return embedding_matrix, word_to_index, observations, classification_model
+    return embedding_matrix, word_to_index, observations, label_encoder, classification_model
 
 
-def load(embedding_matrix, word_to_index, observations, network):
+def load(embedding_matrix, word_to_index, observations, label_encoder, network):
     # TODO Docstring
 
     # TODO Output observations with true labels, expected labels
@@ -169,13 +171,13 @@ def main():
 
     # Transform
     observations = cPickle.load(open('../data/pickles/posts_extract.pkl'))
-    embedding_matrix, word_to_index, observations = transform(embedding_matrix, word_to_index, observations)
+    embedding_matrix, word_to_index, observations, label_encoder = transform(embedding_matrix, word_to_index, observations)
 
     # Model
-    embedding_matrix, word_to_index, observations, network = model(embedding_matrix, word_to_index, observations)
+    embedding_matrix, word_to_index, observations, label_encoder, network = model(embedding_matrix, word_to_index, observations, label_encoder)
 
     # Load
-    load(embedding_matrix, word_to_index, observations, network)
+    load(embedding_matrix, word_to_index, observations, label_encoder, network)
 
     pass
 
